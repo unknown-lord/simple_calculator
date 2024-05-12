49a6172763837711a4bb4a5a6ad80895e63b84a6
unknown-lord <128253795+unknown-lord@users.noreply.github.com>
2024-05-13 02:06:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -24,3 +24,12 @@ arr.forEach(button => {
     })
 
 })
+
+function onlyNumberKey(evt) {
+ 
+            // Only ASCII character in that range allowed
+            let ASCIICode = (evt.which) ? evt.which : evt.keyCode
+            if (ASCIICode > 31 && (ASCIICode < 48 || ASCIICode > 57))
+                return false;
+            return true;
+        }
