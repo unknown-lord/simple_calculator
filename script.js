1c3ab71c7fbda891111d73bfaae90d0a983b0e17
unknown-lord <128253795+unknown-lord@users.noreply.github.com>
2024-05-13 02:12:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ function onlyNumberKey(evt) {
  
             // Only ASCII character in that range allowed
             let ASCIICode = (evt.which) ? evt.which : evt.keyCode
-            if (ASCIICode > 31 && (ASCIICode < 48 || ASCIICode > 57))
+            if (ASCIICode > 31 && (ASCIICode < 40 || ASCIICode > 57))
                 return false;
             return true;
         }
